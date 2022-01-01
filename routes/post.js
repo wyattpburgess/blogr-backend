d3d7b773e357aa0f7db6310da92374a75e2e1a05
wyatt <wyattpburgess@gmail.com>
2022-01-01 14:12:34 -0500

    Create and view post functionality

--- routes/post.js
+++ routes/post.js
@@ -1,22 +1,27 @@
-const express = require('express');
-const { body } = require('express-validator');
+const express = require("express");
+const { body } = require("express-validator");
 
-const postController = require('../controllers/post');
+const postController = require("../controllers/post");
 
 const router = express.Router();
 
+// GET /post/posts
+router.get("/posts", postController.getPosts);
+
 // POST /post/add-new
 router.post(
-  '/add-new',
+  "/add-new",
   [
-    body('title')
-      .trim()
-      .isLength({ min: 5 }),
-    body('body')
-      .trim()
-      .isLength({ min: 5 })
+    body("title").trim().isLength({ min: 5 }),
+    body("body").trim().isLength({ min: 5 }),
   ],
   postController.createPost
 );
+
+// GET post/post-ids
+router.get("/post-ids", postController.getPostIds);
+
+// GET /post/:postId
+router.get("/:postId", postController.getPost);
 
 module.exports = router;
